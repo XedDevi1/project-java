1b1f6743564636d2b7931c9ddb64e3d7d7279c12
XedDevi1 <vadim.presman@gmail.com>
2025-12-06 00:26:08 +0100

    refactor: use constructor instead of lombok

--- src/main/java/com/casino/slot/config/SecurityConfig.java
+++ src/main/java/com/casino/slot/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.casino.slot.config;
 
 import com.casino.slot.service.UserDetailsServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,10 +14,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final UserDetailsServiceImpl userDetailsService;
+
+    public  SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
